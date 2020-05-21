c74ded2fd2d3493f2932534f60161784f563a1f6
Emanuel Tesoriello <>
2020-05-21 17:04:52 +0200

    Update UI/UX

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const RESOURCES = {
   "index.html": "3c2178602ed219892cab111d6a769ed4",
 "/": "3c2178602ed219892cab111d6a769ed4",
 "images/signin_image.jpg": "04181ad2157e038242875b1112036dac",
-"main.dart.js": "1969a7cc44bfa3885167be602c2ba427",
+"main.dart.js": "d922c234b2b3f6d1dc73aee28f1b83ff",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/login_signup_btn_icon.png": "9c87f723ba1143aab0ef00fb47a3fa88",
 "icons/google_icon.png": "cc94506823ea13c17d20148ea85aa28b",
