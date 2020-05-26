ce9573ad58845a2e2c4c9331a85039f767e12f50
Emanuel Tesoriello <>
2020-05-26 13:42:06 +0200

    Update website Fixed bugs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const RESOURCES = {
   "index.html": "3c2178602ed219892cab111d6a769ed4",
 "/": "3c2178602ed219892cab111d6a769ed4",
 "images/signin_image.jpg": "04181ad2157e038242875b1112036dac",
-"main.dart.js": "ad28c28437716bb2f82f7e60fba8904f",
+"main.dart.js": "cb36c49dc8883c11b5946736e3aa7893",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/login_signup_btn_icon.png": "9c87f723ba1143aab0ef00fb47a3fa88",
 "icons/google_icon.png": "cc94506823ea13c17d20148ea85aa28b",
